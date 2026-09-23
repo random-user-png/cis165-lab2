1abef56091df227378c6936f0d4a2f263df09a4d
random-user-png <eszeliga@me.bergen.edu>
2026-09-23 13:24:24 -0400

    Add files via upload

---
+++ mpg.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    float gallons = 8;
+    float miles = 5;
+    float mpg;
+    mpg = miles / gallons;
+    std::cout<<"Output: "<<mpg<<" miles per gallon";
+
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int integer1 = 50;
+    int integer2 = 100;
+    int total;
+    total = integer1 + integer2;
+    std::cout<<"Total: "<<total;
+
+    return 0;
+}
